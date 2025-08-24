2f649a95f696a1b364fedf750aa347c5494c46c9
Salah <salaheddine.ghamri@yahoo.com>
2025-08-24 15:02:30 +0200

    enhanced git

--- git_utils.lua
+++ git_utils.lua
@@ -1,8 +1,38 @@
+-- Author: S.Ghamri
+-- mod-version:3
 local core = require "core"
 local common = require "core.common"
 local system = require "system"
 local process = require "process"
 
+local M = {}
+
+-- m: queue
+local git_queue = {} -- single thread queue
+local git_busy = false
+local function process_next_git_task()
+  if #git_queue == 0 then
+    git_busy = false
+    return
+  end
+
+  git_busy = true
+  local task = table.remove(git_queue, 1)
+
+  core.add_thread(function()
+    task() 
+    process_next_git_task() -- call next task in queue
+  end)
+end
+
+local function enqueue_git_task(task)
+  table.insert(git_queue, task)
+  if not git_busy then
+    process_next_git_task()
+  end
+end
+
+-- m: git process
 local function execute_git(repo_root, args_table)
   local command = { "git", "-C", repo_root }
   for _, arg in ipairs(args_table) do table.insert(command, arg) end
@@ -10,38 +40,45 @@ local function execute_git(repo_root, args_table)
   local git_process = process.start(command)
   if not git_process then
     core.error("git: failed to start process for %s, cmd: %s", repo_root, table.concat(command, " "))
-    return ""
+    return "", -1, ""
   end
 
-  while git_process:running() do coroutine.yield(0.1) end
+  local stdout_output, stderr_output = "", ""
+  while git_process:running() do
+    coroutine.yield() 
+    stdout_output = stdout_output .. (git_process:read_stdout() or "")
+    stderr_output = stderr_output .. (git_process:read_stderr() or "")
+  end
 
-  local stdout_output = git_process:read_stdout() or ""
-  local stderr_output = git_process:read_stderr() or ""
-  local exit_code = git_process:wait()
-
+  local exit_code = git_process:returncode() or -1
   if exit_code ~= 0 then
     core.warn("git: command failed (%d) for %s, cmd: %s", exit_code, repo_root, table.concat(command, " "))
-    if #stderr_output > 0 then core.warn("git: stderr: %s", stderr_output:match("^[^\n]+")) end
-    return ""
+    if #stderr_output > 0 then
+      core.warn("git: stderr: %s", stderr_output:match("^[^\n]+"))
+    end
   end
-  return stdout_output
+
+  return stdout_output, exit_code, stderr_output
 end
 
 local function get_repo_root(filepath)
   if not filepath then return nil end
   local current_dir = common.dirname(filepath)
+  -- check all parent folders
   while current_dir and current_dir ~= "" and current_dir ~= "/" do
-    if system.get_file_info(current_dir .. "/.git") then return current_dir end
+    local info = system.get_file_info(current_dir .. "/.git")
+    if info and info.type == "dir" then
+      return current_dir
+    end
     current_dir = common.dirname(current_dir)
   end
   return nil
 end
 
-local M = {}
-
--- TODO: we can get more then branch name
+-- getters ------------------------------------------------------------------------------
+-- m: current branch
 function M.get_branch(filepath, callback)
-  core.add_thread(function()
+  enqueue_git_task(function()
     local branch_name = ""
     local repo_root = filepath and get_repo_root(filepath)
     if repo_root then
@@ -52,5 +89,25 @@ function M.get_branch(filepath, callback)
     end
   end)
 end
+
+-- m: stats
+function M.get_diff_stats(filepath, callback)
+  enqueue_git_task(function()
+    local repo_root = filepath and get_repo_root(filepath)
+    local added, deleted = 0, 0
+    if repo_root and filepath then
+      local relpath = filepath:sub(#repo_root + 2) -- remove root + "/"
+      local output = execute_git(repo_root, { "diff", "--numstat", relpath })
+      for plus, minus in output:gmatch("(%d+)%s+(%d+)") do
+        added = added + tonumber(plus)
+        deleted = deleted + tonumber(minus)
+      end
+    end
+    if type(callback) == "function" then
+      callback(added, deleted)
+    end
+  end)
+end
+----------------------------------------------------------------------------------------
 
 return M
--- init.lua
+++ init.lua
@@ -1,55 +1,102 @@
+-- Author: S.Ghamri
 -- mod-version:3
 local core = require "core"
 local View = require "core.view"
 local style = require "core.style"
-local git = require "plugins.lite-xl-miniheader.git_utils"
+local git = require((...) .. ".git_utils")
+local config = require "core.config"
 
--- TODO: revise how git process is opened
--- TODO: define faces for the minibuffer
--- TODO: find a way to make sections optional in config
+------------------------------------------------------------------------------
+-- TODO: define faces for the minibuffer. each section can use different color
+-- TODO: make sections optional
+-- DONE: add config
+-- DONE: revise how git process is opened
+------------------------------------------------------------------------------
 
--- reducing the git process frequency
+config.plugins.header = {
+  last_git_time = 0,
+  git_interval = 1.0,
+  height = 35
+}
+
 local last_git_time = 0
 local git_interval = 1.0
+local git_status = {}
 
--- Override the TitleView class entirely
 local TitleView = require "core.titleview"
 
--- TODO: use directory instead of file
-local git_status = {}  
-
--- Save original methods
 local original_new = TitleView.new
 local original_update = TitleView.update
 local original_draw = TitleView.draw
 
--- Override the constructor
 function TitleView:new()
   original_new(self)
-   self.size.y = 35  -- Set our desired height
+  self.size.y = config.plugins.header.height
 end
 
--- Override the update method
 function TitleView:update()
   original_update(self)
-  self.size.y = 35  -- Keep our height
+  self.size.y = config.plugins.header.height
 end
 
--- Override the draw method with our MiniHeader functionality
-function TitleView:draw() 
+local function shorten_path(path, max_len)
+  if not path or #path <= max_len then return path end 
+
+  path = path:gsub("\\", "/")
+
+  local home = os.getenv("HOME")
+  if home and path:sub(1, #home) == home then
+    path = "~" .. path:sub(#home + 1)
+  end
+
+  local parts = {}
+  local prev_part
+  for part in path:gmatch("[^/]+") do
+    if prev_part then
+      local first = prev_part:sub(1, 1)
+      if first == "." then
+        table.insert(parts, prev_part:sub(1, 2))
+      else
+        table.insert(parts, first)
+      end
+    end
+    prev_part = part
+  end
+
+  table.insert(parts, prev_part)
+  local shortened = table.concat(parts, "/")
+  return shortened
+end
+ 
+local function register_git_branch(branch_name, filename)
+  git_status[filename] = git_status[filename] or {}  
+  if branch_name ~= "" then
+    git_status[filename].branch = string.format(" %s", branch_name)
+  else
+    git_status[filename].branch = ""
+  end
+end
+
+local function register_git_stat(plus, minus, filename)
+  git_status[filename] = git_status[filename] or {}  
+  git_status[filename].plus = plus or 0
+  git_status[filename].minus = minus or 0
+end
+
+function TitleView:draw()
   original_draw(self)
   local x, y, w, h = self:get_content_bounds()
-  
-  -- Determine if current view is active and if document is modified
+
   local is_modified = false
   local doc_name = "No Document"
-  
+
   if core.active_view and core.active_view.doc then
     is_modified = core.active_view.doc:is_dirty()
     doc_name = core.active_view.doc:get_name()
   end
-  
-  -- Choose colors based on modified state
+
+  -- m: colors
+  -- TODO: define a table of colors for each section
   local bg_color, fg_color
   if is_modified then
     bg_color = style.background
@@ -58,66 +105,28 @@ function TitleView:draw()
     bg_color = style.background
     fg_color = style.text
   end
-  
-  -- Draw background
+
   renderer.draw_rect(x, y, w, h, bg_color)
-  
-  -- Get current time
+
   local time_str = os.date("%H:%M:%S")
-  
-  -- show path in short format
-  -- TODO: adapt for windows
-  local function shorten_path(path, max_len)
-    if not path or #path <= max_len then return path end -- shorten only long paths
 
-    -- Normalize path separators
-    path = path:gsub("\\", "/")
-
-    -- Normalize Home
-    local home = os.getenv("HOME")
-    if home and path:sub(1, #home) == home then
-     path = "~" .. path:sub(#home + 1)
-    end
-
-    local parts = {}
-    local prev_part
-    for part in path:gmatch("[^/]+") do
-      if prev_part then
-         local first = prev_part:sub(1, 1)
-         if first == "." then
-           table.insert(parts, prev_part:sub(1, 2))
-         else
-           table.insert(parts, first)
-         end
-       end
-       prev_part = part
-    end
-
-    table.insert(parts, prev_part)
-    local shortened = table.concat(parts, "/")
-    return shortened
-  end
-  
   local buffer_path = "[No File]"
   if core.active_view and core.active_view.doc and core.active_view.doc.abs_filename then
-  buffer_path = shorten_path(core.active_view.doc.abs_filename, 40)
+    buffer_path = shorten_path(core.active_view.doc.abs_filename, 40)
   end
-  
-  -- Add modified indicator
+
   if is_modified then
     buffer_path = " ● " .. buffer_path
   else
     buffer_path = "    " .. buffer_path
   end
-  
-  -- Get current line and column
+
   local location = ""
   if core.active_view and core.active_view.doc then
     local line, col = core.active_view.doc:get_selection()
     location = string.format("L%d C%d", line, col)
   end
-  
-  -- Get position percentage
+
   local position = ""
   if core.active_view and core.active_view.doc then
     local line, _ = core.active_view.doc:get_selection()
@@ -126,66 +135,61 @@ function TitleView:draw()
     position = string.format("%d%%", percent)
   end
 
-  -- Get file encoding
   local file_encoding = ""
   if core.active_view and core.active_view.doc then
-  file_encoding = core.active_view.doc.encoding or "utf-8"
+    file_encoding = core.active_view.doc.encoding or "utf-8"
   end
-  
-  -- Get file type/syntax
+
   local syntax_name = ""
   if core.active_view and core.active_view.doc and core.active_view.doc.syntax then
     syntax_name = core.active_view.doc.syntax.name or ""
   end
 
-  local function my_branch_handler(branch_name, filename)
-  if branch_name ~= "" then
-      git_status[filename] = string.format(" %s", branch_name)
-  else
-      git_status[filename] = ""
-  end
+  -- m: git related
+  if core.active_view and core.active_view.doc and core.active_view.doc.abs_filename then
+    local file = core.active_view.doc.abs_filename
+
+    local now = system.get_time()
+    if (now - last_git_time < git_interval) then
+        git.get_branch(file, function(branch) register_git_branch(branch, file) end)
+        git.get_diff_stats(file, function(plus, minus)  register_git_stat(plus, minus, file) end)
+    end
+
+    last_git_time = now
   end
 
-  -- Update logic
-  if core.active_view and core.active_view.doc and core.active_view.doc.abs_filename then
-  local file = core.active_view.doc.abs_filename
-  -- TODO: called at draw speed :/
-  local now = system.get_time()
-  if (now - last_git_time < git_interval) then
-     git.get_branch(file, function(branch) my_branch_handler(branch, file) end)
-  end
-  last_git_time = now
-  end
-  
-  -- Build left section
+
   local left_parts = {}
-  table.insert(left_parts, time_str) -- TODO: make this optional
+  table.insert(left_parts, time_str) 
   table.insert(left_parts, buffer_path)
   local left_section = table.concat(left_parts, " ")
-  
-  -- Build right section
+
   local right_parts = {}
 
   local file = core.active_view and core.active_view.doc and core.active_view.doc.abs_filename
-  local git_branch = git_status[file]
+  local info = git_status[file] or {}
+  local added = info.plus or 0
+  local deleted = info.minus or 0
+  local git_branch = info.branch
+  local git_stat = (added ~= 0 and deleted ~=0 and string.format("+%d/-%d", added, deleted)) or "" 
 
   if git_branch ~= "" then table.insert(right_parts, git_branch) end
+  if git_stat ~= "" then table.insert(right_parts, git_stat) end
   if syntax_name ~= "" then table.insert(right_parts, syntax_name) end
   if file_encoding ~= "" then table.insert(right_parts, file_encoding) end
   if location ~= "" then table.insert(right_parts, location) end
   if position ~= "" then table.insert(right_parts, position) end
   local right_section = table.concat(right_parts, " ")
-  
-  -- Draw left section
+
   local font = style.font
   renderer.draw_text(font, left_section, x + 10, y + (h - font:get_height()) / 2, fg_color)
-  
-  -- Draw right section (right-aligned)
+
   if right_section ~= "" then
     local text_w = font:get_width(right_section)
     renderer.draw_text(font, right_section, x + w - text_w - 10, y + (h - font:get_height()) / 2, fg_color)
   end
 end
 
--- Make sure the title view is always visible
 core.show_title_bar(true)
+
+
